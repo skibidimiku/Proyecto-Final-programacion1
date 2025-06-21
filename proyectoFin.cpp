5c3303eb2d64844ad059f402df1854d16a9b227c
skibidimiku <idkwim19@gmail.com>
2025-06-21 07:45:13 -0600

    Versión 1.2 con structs
    Esta actualización cambia los enums por structs

--- proyectoFin.cpp
+++ proyectoFin.cpp
@@ -10,30 +10,20 @@
 
 using namespace std;
 
-typedef enum { //Cuatro enum diferentes para intentar borrarlos despues de que salga uno para evitar cartas repetidas
-    Picas2 = 2, Picas3, Picas4, Picas5, Picas6, Picas7, Picas8,
-    Picas9, Picas10, PicasJ = 11, PicasQ = 12, PicasK = 13, PicasAs = 1
-} PaloP;
+enum Tipo {Picas = 1, Corazones, Treboles, Diamantes};
 
-typedef enum { 
-    Coraz2 = 2, Coraz3, Coraz4, Coraz5, Coraz6, Coraz7, Coraz8,
-    Coraz9, Coraz10, CorazJ = 11, CorazQ = 12, CorazK = 13, CorazAs = 1
-} PaloC;
-
-typedef enum { 
-    Trebol2 = 2, Trebol3, Trebol4, Trebol5, Trebol6, Trebol7, Trebol8,
-    Trebol9, Trebol10, TrebolJ = 11, TrebolQ = 12, TrebolK = 13, TrebolAs = 1
-} PaloT;
-
-typedef enum { 
-    Diaman2 = 2, Diaman3, Diaman4, Diaman5, Diaman6, Diaman7, Diaman8,
-    Diaman9, Diaman10, DiamanJ = 11, DiamanQ = 12, DiamanK = 13, DiamanAs = 1
-} PaloD;//aqui termina los enum
+struct baraja{
+    Tipo hello;
+    string NomVal;
+    int valcar;
+};
 
 struct Partida{//aqui inicia la parte para la funcion de guardae en un archivo la partida
     char jugador[50];
     char fecha[20];
     char ganador[15];
+    baraja* barjug;
+    int nca=2;
 };
 
 void obtenerNomCar(int val, string& nombre){//se enitende que esto procesa el numero del enum y para poder mostrar las cartas de fugura
@@ -51,8 +41,6 @@ void obtenerNomCar(int val, string& nombre){//se enitende que esto procesa el nu
         case 11: nombre = "J"; break;
         case 12: nombre = "Q"; break;
         case 13: nombre = "K"; break;
-        
-        
      }
 }
 
@@ -63,8 +51,7 @@ void ValAs(int& val){//esta funcion manda a preguntar que valor quiere darle al 
     } while(val != 1 && val != 11);
 }
 
-void generarCarta(int& valorSumar, int& sumTot){//aqui se genera la carta, creo que aqui va la condicion de 21
-	
+void generarCarta(int& valorSumar, int& sumTot, struct Partida* jug){//aqui se genera la carta, creo que aqui va la condicion de 21
     int palo=0; 
     int val=0; 
     int auxAs=0;
@@ -72,90 +59,95 @@ void generarCarta(int& valorSumar, int& sumTot){//aqui se genera la carta, creo 
     int valCam=0;
     
     if(sumTot<21){
-    palo = rand()%4;
-     val = rand()%13+1; 
+        for (int i = 0; i < jug->nca; i++){
+            struct baraja *cart = &(jug->barjug[i]);
+            palo = 1+rand()%(4-1+1);
+            val = 1+rand()%(13-1+1);
 
-    string nomPal;
-    switch(palo){
-        case 0: nomPal = "Picas"; break;
-        case 1: nomPal = "Corazones"; break;
-        case 2: nomPal = "Treboles"; break;
-        case 3: nomPal = "Diamantes"; break;
-    }
+            switch(palo){
+                case 1: cart->hello = Picas; break;
+                case 2: cart->hello = Corazones; break;
+                case 3: cart->hello = Treboles; break;
+                case 4: cart->hello = Diamantes; break;
+            }
 
-    string nomVal;
-    obtenerNomCar(val, nomVal);
+            string nomVal;
+            obtenerNomCar(val, nomVal);
+            switch(jug->barjug[i].hello){ 
+                case Picas: cout << "Carta generada: " << nomVal << " de Picas"<< endl; break;//aqui se imprime el numero de la carta y se acompaña con su palo
+                case Corazones: cout << "Carta generada: " << nomVal << " de Corazones"<< endl; break;//aqui se imprime el numero de la carta y se acompaña con su palo
+                case Treboles: cout << "Carta generada: " << nomVal << " de Treboles" << endl; break;//aqui se imprime el numero de la carta y se acompaña con su palo
+                case Diamantes: cout << "Carta generada: " << nomVal << " de Diamantes" << endl; break;//aqui se imprime el numero de la carta y se acompaña con su palo
+            }
 
-    cout << "Carta generada: " << nomVal << " de " << nomPal << endl;//aqui se imprime el numero de la carta y se acompaña con su palo
-
-    
-    	
-	if(val==1){
-		valorSumar += val;
-		auxAs=val;
-	}
+	        if(val==1){
+		        valorSumar += val;
+		        auxAs=val;
+	        }
 	
 	
-    if (auxAs == 1){
+            if (auxAs == 1){
     
-      if (val >= 11 && val <= 13){
-        valorSumar += 10; 
-        ValAs(valCam);
-        auxAs2=valCam;
-        valorSumar+= -auxAs+auxAs2;
+                if (val >= 11 && val <= 13){
+                    valorSumar += 10; 
+                    ValAs(valCam);
+                    auxAs2=valCam;
+                    valorSumar+= -auxAs+auxAs2;
         
-    } else if (val >= 2 && val <= 10){
-        valorSumar += val;
-        ValAs(valCam);
-        auxAs2=valCam;
-        valorSumar=valorSumar -auxAs+ auxAs2;
-    }
-    
-
-} 
-else {
-    // 
-    if (val >= 11 && val <= 13) {
-        valorSumar = 10;
-    } else if (val >= 2 && val <= 10) {
-        valorSumar = val;
-    }
-}
+                } else if (val >= 2 && val <= 10){
+                    valorSumar += val;
+                    ValAs(valCam);
+                    auxAs2=valCam;
+                    valorSumar=valorSumar -auxAs+ auxAs2;
+                }
+            
+            } else {
+                if (val >= 11 && val <= 13) {
+                    valorSumar = 10;
+                }else if (val >= 2 && val <= 10) {
+                    valorSumar = val;
+                }
+            }
 	 
 
-    sumTot += valorSumar;
+            sumTot += valorSumar;
 
-    cout << "Valor sumado: " << valorSumar << endl;
-    cout << "Total acumulado: " << sumTot << endl;
-}else{if(sumTot>21){
+            cout << "Valor sumado: " << valorSumar << endl;
+            cout << "Total acumulado: " << sumTot << endl;
+        }
+    }
+}
+    
 
-	//supongo que aqui directamente pierde, pero la vamos a comparar con la maquina para despues saber el valor de la variable ganador
-}
-}
-}
-
-void jugarCartas(int& SumJug){
+void jugarCartas(int& SumJug, struct Partida* jug){
     char op;
     int valorSumar;
 
     SumJug = 0;
 
     cout << "\n--- Repartiendo tus dos primeras cartas ---" << endl;
-    generarCarta(valorSumar, SumJug);
-    generarCarta(valorSumar, SumJug);
+    generarCarta(valorSumar, SumJug, jug);
 
     do {
-        cout << "Quieres sacar otra carta? (s/n): ";
+        cout << "Hit o Stand (H/S):";
         cin >> op;
 
-        if (op == 's' || op == 'S') {
-            generarCarta(valorSumar, SumJug);
+        if (op == 'h' || op == 'H') {
+            generarCarta(valorSumar, SumJug, jug);
+         }else if(op == 's' || op == 'S'){
+            cout<<"¿Quieres abandonar (s/n)?";
+            cin>>op;
+            if (op == 's' || op == 'S'){
+                break;
+            }
+         }else{
+            cout<<"Esta opcion no existe intente de nuevo"<<endl;
          }
 
-    } while(op == 's' || op == 'S');
+    } while(SumJug<21);
 }
 
-void generarDatosPartida(Partida partida[]){
+void generarDatosPartida(Partida* partida){
     cout << "Ingresa tu nombre (sin espacios): ";
     cin >> partida[0].jugador;
 
@@ -165,13 +157,13 @@ void generarDatosPartida(Partida partida[]){
 
 int main(){
     srand(time(0));
-    Partida partida[TAM];
+    Partida partida;
     int SumJug;
 
-    generarDatosPartida(partida);
-    jugarCartas(SumJug);
+    generarDatosPartida(&partida);
+    jugarCartas(SumJug, &partida);
 
-    cout << "\n " << partida[0].jugador << endl;
+    cout << "\n " << partida.jugador << endl;
     cout << "Juego terminado. Total final acumulado: " << SumJug << endl;
 
     return 0;
